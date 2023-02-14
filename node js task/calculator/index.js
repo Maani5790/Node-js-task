5b00a928bbfd069204935108c107adde62758ac7
Abdul Rehman <114314272+Maani5790@users.noreply.github.com>
2023-02-14 14:16:33 +0500

    Add files via upload

---
+++ node js task/calculator/index.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')();
+ const input1 = prompt("Enter First Number");
+ const  input2 = prompt("Enter Second Number");
+console.log(`The input of ${input1}  ${input2} is ${input1 + input2}`);
+
